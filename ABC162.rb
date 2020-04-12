5ce0a2b2638cc2d808d6d3fc883fc19310c2d2be
shwe-yuji <okumura19943210yuji@gmail.com>
2020-04-13 00:43:24 +0900

    ABC162問題3no

--- ABC162.rb
+++ ABC162.rb
@@ -19,3 +19,13 @@ end
 p nums.inject(:+)
 
 # 問題3(不正解)
+n = gets.to_i
+sum = 0
+1.upto(n) do |a|
+  1.upto(n) do |b|
+    1.upto(n) do |c|
+      sum += a.gcd(b).gcd(c)
+    end
+  end
+end
+puts sum

0252f756203619f46c77c5045294b181d39c823f
shwe-yuji <okumura19943210yuji@gmail.com>
2020-04-19 22:53:54 +0900

    ABC163を追加

---
+++ ABC163.rb
@@ -0,0 +1,28 @@
+#問題1
+r = gets.to_f
+puts (r * 2) * 3.14
+
+#問題2
+n, m = gets.split(" ").map(&:to_i)
+hw = gets.split(" ").map(&:to_i)
+vacation = n - hw.inject(:+)
+if vacation < 0
+  puts -1
+else
+  puts vacation
+end
+
+#問題3(制限時間超過)
+#原因　Array.countが多くの時間を使うため
+n = gets.to_i
+nums = gets.split.map(&:to_i)
+
+# 要素数nで初期値が0の配列を準備
+ans = Array.new(n, 0)
+nums.each do |num|
+  # ansの(num - 1)番目に1ずつ足していく
+  ans[num - 1] += 1
+end
+ans.each do |a|
+  puts a
+end

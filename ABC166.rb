78b0c995162b195097252c8c1ee6225c34b0ddee
shwe-yuji <okumura19943210yuji@gmail.com>
2020-05-04 19:56:05 +0900

    ABC166修正

--- ABC166.rb
+++ ABC166.rb
@@ -27,21 +27,18 @@ else
 end
 
 #問題3
-n, m = gets.to_s.split.map{|t|t.to_i}
-h    = [0] + gets.to_s.split.map{|t|t.to_i}
-
-ans = [1] * (n+1)
-ans[0] = 0
-
-m.times do
+n, m = gets.split(" ").map(&:to_i)
+h = gets.split(" ").map(&:to_i)
+check = Array.new(n, 1)
+for i in 1..m
   a, b = gets.split(" ").map(&:to_i)
-  if h[a] < h[b]
-    ans[a] = 0
-  elsif h[a] > h[b]
-    ans[b] = 0
-  else
-    ans[a] = ans[b] = 0
+  if h[a-1] < h[b-1]
+    check[a-1] = 0
+  elsif h[a-1] > h[b-1]
+    check[b-1] = 0
+  elsif h[a-1] = h[b-1]
+    check[a-1] = 0
+    check[b-1] = 0
   end
-  p ans
 end
-puts ans.sum
+puts check.sum

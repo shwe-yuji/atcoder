1de8e66367d4fcf9406f302c2590ae9b8d501599
shwe-yuji <okumura19943210yuji@gmail.com>
2020-05-04 16:57:18 +0900

    ABC166修正

--- ABC166.rb
+++ ABC166.rb
@@ -1,47 +1,47 @@
 #問題1
-# s = gets.chomp
-# if s == "ABC"
-#   puts "ARC"
-# else
-#   puts "ABC"
-# end
+s = gets.chomp
+if s == "ABC"
+  puts "ARC"
+else
+  puts "ABC"
+end
 
 #問題2
-# n, k = gets.split(" ").map(&:to_i)
-# hash = {}
-# for i in 0..n-1
-#   hash[i+1] = []
-# end
-# for i in 1..k
-#   sweets_num = gets.to_i
-#   holders = gets.split(" ").map(&:to_i)
-#   holders.each do |h|
-#     hash[h].push(sweets_num)
-#   end
-# end
-
-# if hash.group_by{|k, v| v.length > 0}[false].nil?
-#   puts 0
-# else
-#   puts hash.group_by{|k, v| v.length > 0}[false].length
-# end
-
-#問題3
-n, m = gets.split(" ").map(&:to_i)
-heights = gets.split(" ").map(&:to_i)
-# hash = {}
-# for i in 1..n
-#   hash[i] = heights[i-1]
-# end
-path_hash = {}
-for i in 1..m
-  path = gets.split(" ").map(&:to_i)
-  if path_hash[path[0]]
-    path_hash[path[0]].push(path[1])
-  else
-    path_hash[path[0]] = []
-    path_hash[path[0]].push(path[1])
+n, k = gets.split(" ").map(&:to_i)
+hash = {}
+for i in 0..n-1
+  hash[i+1] = []
+end
+for i in 1..k
+  sweets_num = gets.to_i
+  holders = gets.split(" ").map(&:to_i)
+  holders.each do |h|
+    hash[h].push(sweets_num)
   end
 end
 
-p path_hash.group_by
+if hash.group_by{|k, v| v.length > 0}[false].nil?
+  puts 0
+else
+  puts hash.group_by{|k, v| v.length > 0}[false].length
+end
+
+#問題3
+n, m = gets.to_s.split.map{|t|t.to_i}
+h    = [0] + gets.to_s.split.map{|t|t.to_i}
+
+ans = [1] * (n+1)
+ans[0] = 0
+
+m.times do
+  a, b = gets.split(" ").map(&:to_i)
+  if h[a] < h[b]
+    ans[a] = 0
+  elsif h[a] > h[b]
+    ans[b] = 0
+  else
+    ans[a] = ans[b] = 0
+  end
+  p ans
+end
+puts ans.sum

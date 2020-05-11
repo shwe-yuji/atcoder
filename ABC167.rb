40e0a3152b1a7693cd9b9221f8d5f5f1227f413e
shwe-yuji <okumura19943210yuji@gmail.com>
2020-05-12 04:39:32 +0900

    ABC167修正

--- ABC167.rb
+++ ABC167.rb
@@ -37,9 +37,10 @@ end
 # 写経
 # 2のn乗通りの全検索、ビット数(1か0かで判別するスイッチのようなもの)
 # この問題では買うか、買わないかの2択で判断
-
 n, m, x = gets.split(" ").map(&:to_i)
+# ↓こっちの方が、行数が少なく、見やすい！
 cas = Array.new(n){ gets.to_s.split.map{|e|e.to_i} }
+
 inf = 10 ** 7
 ans = inf
 (2**n).times do |bit|
@@ -55,3 +56,29 @@ ans = inf
   ans = [ans, cnt].min if rikaido.all?{ |r| r >= x } 
 end
 puts ans == inf ? -1 : ans
+
+#オリジナル
+n, m, x = gets.split(" ").map(&:to_i)
+books = Array.new(n){ gets.split(" ").map(&:to_i) }
+
+check = []
+for i in 1..2 ** n
+  bit = i
+  price = 0
+  understanding = Array.new(m){ 0 }
+  books.each_with_index do |book, j|
+    if bit[j] == 1
+      price += book[0]
+      (1..m).each{|j| understanding[j-1] += book[j]}
+    end
+  end
+  if understanding.all?{|r| r >= x}
+    check << price
+  end
+end
+
+if check.empty? 
+  puts -1 
+else
+  puts check.min
+end
